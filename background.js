a6ca8513db15e414dd38e12add70c5040d29d621
Christian Andersen <c.andersen2012@gmail.com>
2018-10-20 11:43:27 -0500

    22: Adding google analytics report

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
+// Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-46757863-5']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+// Functions
 function listener(message) {
   console.log(JSON.stringify(message));
   chrome.tabs.sendMessage(message.tabId, message.title);
@@ -13,7 +25,13 @@ function omniboxListener(title) {
   });
 }
 
+function omniboxTrackingListener(title) {
+  _gaq.push(['_trackEvent', 'changeTabName', 'omniboxEnter']);
+}
+
+// Listeners
 chrome.omnibox.onInputChanged.addListener(omniboxListener);
+chrome.omnibox.onInputEntered.addListener(omniboxTrackingListener);
 chrome.runtime.onMessage.addListener(listener);
 
 chrome.runtime.onInstalled.addListener(function() {
--- popup.js
+++ popup.js
@@ -1,7 +1,20 @@
+// Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-46757863-5']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+// Retrieve Elements
 let changeNameButton = document.getElementById('changeNameButton')
 let tabNameElem = document.getElementById("tabNameInput");
 let form = document.getElementById("tabForm");
 
+// Event Functions
 changeNameButton.onclick = function(element) {
   changeTabName();
 };
@@ -15,6 +28,7 @@ document.onload = function() {
 }
 
 function changeTabName() {
+  _gaq.push(['_trackEvent', 'changeTabName', "popup"]);
   let title = tabNameElem.value;
   console.log(tabNameElem.value);
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
