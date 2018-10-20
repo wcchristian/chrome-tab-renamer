a8cf40ddb876b1622e3a46d80a119e019ecb9ce2
Christian Andersen <c.andersen2012@gmail.com>
2018-10-19 20:47:46 -0500

    3: Adding custom title

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ A tab renamer extension for google chrome. This extension will allow you
 ## Tutorial
 For practice, I included the chrome extension tutorial in the tutorial folder of this repo.
 [Tutorial Link](https://developer.chrome.com/extensions/getstarted)
+
+# How to use
+1. Navigate to a tab that you wish to rename
+1. Click the Chrome Tab Renamer extension button.
+1. Type the new name for your tab
+1. Click the "Change Tab Name" or press enter to rename the tab.
+
+# Screenshots
+![Image of Tabs](screenshots/tabs.png)
+![Image of Input](screenshots/enter.png)
--- popup.js
+++ popup.js
@@ -1,16 +1,26 @@
 let changeNameButton = document.getElementById('changeNameButton')
-let titleElem = document.getElementById("title");
-
-chrome.storage.sync.get('tabName', function(data) {
-  titleElem.innerText = data.tabName;
-});
+let tabNameElem = document.getElementById("tabNameInput");
+let form = document.getElementById("tabForm");
 
 changeNameButton.onclick = function(element) {
-  let title = titleElem.innerText;
+  changeTabName();
+};
+
+document.onload(function() {
+  form.addEventListener('keyup', function(data) {
+    if(data.keyCode === 13) {
+      changeTabName();
+    }
+  })
+})
+
+function changeTabName() {
+  let title = tabNameElem.value;
+  console.log(tabNameElem.value);
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
       tabs[0].id,
       {code: 'document.title = "'+title+'"'} 
     );
   });
-};
+}
